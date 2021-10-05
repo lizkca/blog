bcceeee23d23753bade962e8b445a082157a164d
lizkca <lizkca@outlook.com>
2021-10-05 12:51:22 +0800

    add ablog

--- conf.py
+++ conf.py
@@ -30,7 +30,10 @@ release = 'v1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"
+extensions = [
+    "myst_parser",
+    "ablog",
+    "sphinx.ext.intersphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
